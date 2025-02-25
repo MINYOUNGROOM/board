d775ca7d2cd2cb7282793e99a917c90721c00ed0
김민영 <kmyoffice99@gmail.com>
2025-02-25 17:16:27 +0900

    이미지 업로드 & 다운로드 , 페이징 처리

--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -3,13 +3,12 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 
@@ -21,25 +20,26 @@ public class BoardService {
 
     // 게시물 글 작성
     public void write(Board board, MultipartFile file) throws Exception {
-        String projectPath = System.getProperty("user.dir")+"\\src\\main\\resources\\static\\files"; // 프로젝트 경로를 projectPath에 담는것
+        String projectPath = System.getProperty("user.dir")+"\\board\\src\\main\\resources\\static\\files"; // 프로젝트 경로를 projectPath에 담는것
 
-        // 폴더가 존재하지 않으면 생성
-        File directory = new File(projectPath);
-        if (!directory.exists()) {
-            directory.mkdirs(); // 디렉토리 생성
-        }
 
         UUID uuid = UUID.randomUUID();
         String fileName = uuid + "_" + file.getOriginalFilename(); // 고유한 번호 + 본 파일 이름
         File saveFile = new File(projectPath, fileName);
 
+        System.out.println("projectPath : " + projectPath); // 파일 저장 경로
+
         file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
+
         boardRepository.save(board);
     }
 
     // 게시물 조회
-    public List<Board> getList() {
-        return boardRepository.findAll();
+    public Page<Board> getList(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
 
     // 게시물 상세 조회
@@ -52,6 +52,4 @@ public class BoardService {
     public void delete(Integer id){
         boardRepository.deleteById(id);
     }
-
-
 }
